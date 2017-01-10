225cef2a845ef4c938fb02c3481a2f7fece56c91
kaushik laptop <kaushik.srinivas@gmail.com>
2017-01-10 22:11:07 +0530

    add more handlers

--- apps/hello-world/main.py
+++ apps/hello-world/main.py
@@ -19,6 +19,14 @@ form = """
 	<input name="q">
 	<input type="submit">
 </form>
+<form action="/testform">
+	<input name="q">
+	<input type="submit">
+</form>
+<form action="/testform2">
+	<input name="q">
+	<input type="submit">
+</form>
 """
 
 class MainPage(webapp2.RequestHandler):
@@ -26,7 +34,19 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(form)
 
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
+
+class TestHandler2(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(self.request) # write the http request
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage), # MainPage class handles this request
+    ('/testform', TestHandler),  # TestHandler class handles this request
+     ('/testform2', TestHandler2), # TestHandler2 class handles this request
 ], debug=True)
+
