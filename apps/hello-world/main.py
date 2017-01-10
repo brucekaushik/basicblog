41ed1f82c27b89168720ab2fcce3d938800df78b
kaushik laptop <kaushik.srinivas@gmail.com>
2017-01-10 20:33:33 +0530

    change content type to text/html

--- apps/hello-world/main.py
+++ apps/hello-world/main.py
@@ -23,7 +23,7 @@ form = """
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
         self.response.write(form)
 
 
