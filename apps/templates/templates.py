2a9d8baa765920aab24403d92db1ce592cba762b
kaushik laptop <kaushik.srinivas@gmail.com>
2017-01-15 02:06:50 +0530

    basic shopping list functionality

--- apps/templates/templates.py
+++ apps/templates/templates.py
@@ -19,9 +19,25 @@ form_html = """
 <form action="/">
 	<h2>Add a Food</h2>
 	<input type="text" name="food" value="">
+	%s
 	<input type="submit">
 </form>
 """
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<h2>Add a Food</h2>
+<ul>
+%s
+</ul>
+"""
+
+
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -30,8 +46,23 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler): # inherit MainPage from Handler
 	def get(self):
-		self.write(form_html)
+		output = form_html
+		output_hidden = ""
 
+		items = self.request.get_all("food")
+
+		if items:
+			output_items = ""
+			for item in items:
+				output_hidden += hidden_html % item
+				output_items += item_html % item
+
+			output_shopping = shopping_list_html % output_items
+			output += output_shopping
+
+		output = output % output_hidden
+		
+		self.write(output)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
